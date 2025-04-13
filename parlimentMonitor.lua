011baedf6504cde96680a8ec0b96c378af9e5776
twoThirdsHuman <axelwas07@gmail.com>
2025-04-13 17:41:49 +0200

    added voting

--- parlimentMonitor.lua
+++ parlimentMonitor.lua
@@ -139,13 +139,140 @@ local function displayBill(billName)
     end)
 end
 
+local function floor(num)
+    num = math.floor(num)
+    if num == 0 then
+        return 1
+    end
+    return num
+end
+
+---@param title string
+---@param monitor unknown
+local function writeTitle(title, monitor) 
+    ---@diagnostic disable-next-line: unbalanced-assignments
+    local width, _ = monitor.getSize()
+
+    if #title > width then
+        error("Bill title to long to display")
+    end
+    local startX = floor((width / 2) - (#title / 2))
+    
+    monitor.setCursorPos(startX, 2)
+    monitor.write(title)
+end
+
+---@param monitor unknown
+local function writeForAgainst(monitor)
+    ---@type number, number
+    ---@diagnostic disable-next-line: unbalanced-assignments
+    local width, _ = monitor.getSize()
+    
+    local forText = "for"
+    local againstText = "against"
+    
+    local third = width / 3
+
+    local writeX = floor(third - ((#forText) / 2))
+    monitor.setCursorPos(writeX, 5)
+    ---@diagnostic disable-next-line: undefined-global
+    monitor.setTextColor(colors.green)
+    monitor.write(forText)
+
+    writeX = floor(2 * third - (#againstText / 2))
+    monitor.setCursorPos(writeX, 5)
+    ---@diagnostic disable-next-line: undefined-global
+    monitor.setTextColor(colors.red)
+    monitor.write(againstText)
+
+    ---@diagnostic disable-next-line: undefined-global
+    monitor.setTextColor(colors.white)
+end
+
+
+---@param monitor unknown
+---@param forVotes number
+---@param againstVotes number
+local function writeVotes(monitor, forVotes, againstVotes)
+    ---@type number, number
+    ---@diagnostic disable-next-line: unbalanced-assignments
+    local width, _ = monitor.getSize()
+    
+    local forText = tostring(forVotes)
+    local againstText = tostring(againstVotes)
+    
+    local third = width / 3
+
+    local writeX = floor(third - ((#forText) / 2))
+    monitor.setCursorPos(writeX, 7)
+    ---@diagnostic disable-next-line: undefined-global
+    monitor.setTextColor(colors.green)
+    monitor.write(forText)
+
+    writeX = floor(2 * third - (#againstText / 2))
+    monitor.setCursorPos(writeX, 7)
+    ---@diagnostic disable-next-line: undefined-global
+    monitor.setTextColor(colors.red)
+    monitor.write(againstText)
+
+    ---@diagnostic disable-next-line: undefined-global
+    monitor.setTextColor(colors.white)
+end
+
+---@param title string
+local function displayVote(title)
+    ---@diagnostic disable-next-line: undefined-global
+    local monitor = peripheral.find("monitor")
+    
+    local forVotes, againstVotes = 0, 0
+    writeTitle(title, monitor)
+    writeForAgainst(monitor)
+    writeVotes(monitor, forVotes, againstVotes)
+
+    ---@diagnostic disable-next-line: undefined-global
+    writeTitle(title, term)
+    ---@diagnostic disable-next-line: undefined-global
+    writeForAgainst(term)
+    ---@diagnostic disable-next-line: undefined-global
+    writeVotes(term, forVotes, againstVotes)
+
+    ---@diagnostic disable-next-line: undefined-global
+    term.setCursorPos(1, 10)
+    ---@diagnostic disable-next-line: undefined-global
+    write("Press f to add for vote, a to add against vote, q to quit")
+
+    while true do
+        ---@diagnostic disable-next-line: undefined-global
+        writeVotes(term, forVotes, againstVotes)
+        writeVotes(monitor, forVotes, againstVotes)
+
+        ---@diagnostic disable-next-line: undefined-field, unbalanced-assignments
+        local type, char = os.pullEvent()
+
+        if type == "char" then
+            if char == "q" then
+                return
+            elseif char == "f" then
+                forVotes = forVotes + 1
+            elseif char == "a" then 
+                againstVotes = againstVotes + 1
+            end
+        end
+    end
+end
+
 if option == "display" then
     if arg1 == nil then
-        error("Please specify a bill to display")
+        displayBill("The Constitution")
+    else
+        displayBill(arg1)
     end
-    displayBill(arg1)
 elseif option == "vote" then
-
+    if arg1 == nil then
+        displayVote("Example Act")
+    else
+        displayVote(arg1)
+    end
 elseif option == nil then
     print("Uses:")
     print(" - display [billName]")
