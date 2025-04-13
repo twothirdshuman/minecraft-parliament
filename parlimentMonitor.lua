f7eede07b58dead4267147936477d282f4413679
twoThirdsHuman <axelwas07@gmail.com>
2025-04-13 15:46:33 +0200

    writing

--- parlimentMonitor.lua
+++ parlimentMonitor.lua
@@ -1,4 +1,5 @@
 --- @type string?, string?, string?
+--- @diagnostic disable-next-line: unbalanced-assignments
 local option, arg1, arg2 = ...
 
 ---@param str string
@@ -28,14 +29,51 @@ local function getBillText(billName)
     return billText
 end
 
+---@param text string
+---@param monitor unknown
+local function writeToScreen(text, monitor)
+    --- @type number, number
+    --- @diagnostic disable-next-line: unbalanced-assignments
+    local width, height = monitor.getSize()
+
+    --- @type string[]
+    local words = {}
+
+    for word in string.gmatch(text, "[^ ]+") do
+        table.insert(words, word)
+    end
+
+    monitor.clear()
+    monitor.setCursorPos(1, 1)
+
+    for wordIndex = 1,#words do
+        local word = words[wordIndex]
+        --- @type number, number
+        --- @diagnostic disable-next-line: unbalanced-assignments
+        local x, y = monitor.getCursorPos()
+
+        if (x + #word - 1) > width then
+            monitor.setCursorPos(1, y + 1)
+        end
+
+        for i = 1, #word do
+            local char = string.sub(word, i, i)
+            monitor.write(char)
+            if char == "\n" then
+                monitor.setCursorPos(1, y + 1)
+            end
+        end
+        monitor.write(" ")
+    end
+end
+
 ---@param billName string
 local function displayBill(billName)
     local billText = getBillText(billName)
 
     ---@diagnostic disable-next-line: undefined-global
-    local monitor = peripheral.find("monitor")[1]
-
-    monitor.write(billText)
+    local monitor = peripheral.find("monitor")
+    writeToScreen(billText, monitor)
 end
 
 if option == "display" then
